ee2d3788016391843eb2288db9eaa0053b6b0b07
kksnowflake98 <119512712+kksnowflake98@users.noreply.github.com>
2022-12-05 15:22:46 -0500

    Update streamlit_app.py
    adding some things

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 
 streamlit.title('Testing a title')
+streamlit.header('Testing a header')
+streamlit.text('Testing a line of text 1')
+streamlit.text('Testing aanother line of text')
